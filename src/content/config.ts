592cbb00a016a57b729ade4d6f953adb6b47f4bc
LostoVayne <moises.barrera.dev@gmail.com>
2024-08-26 00:02:30 -0400

    agregadas las colecciones y los staticPaths

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -8,15 +8,24 @@ const blogCollection = defineCollection({
       date: z.date(),
       description: z.string(),
       image: image().refine((img) => img.width > 800, { message: "La imagen debe ser mayor a 800px" }),
-
       // Relacion
-      author: z.string(),
-
+      // author: z.string(),
+      author: reference("author"),
       // Relacion
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
