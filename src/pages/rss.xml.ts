4de8d0efd0dbfde61016adca9272b49b7dcb8685
LostoVayne <moises.barrera.dev@gmail.com>
2024-08-30 01:28:11 -0400

    agrega nueva importacion de xmlns

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site = "https://example.com" }) => {
   const blogPosts = await getCollection("blog");
@@ -9,12 +13,28 @@ export const GET: APIRoute = async ({ params, request, site = "https://example.c
     // stylesheet: "/styles/rss.xsl",
     title: "Astro RSS Feed",
     description: "Un simple blog de pruebas sobre mi con astro",
+    // namespace respectivo
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
     site: site,
-    items: blogPosts.map(({ data, slug }) => ({
+    items: blogPosts.map(({ data, slug, body }) => ({
       title: data.title,
       pubDate: data.date,
       description: data.description,
       link: `posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+
+      customData: `<media:content
+          type="image/${data.image.format === "jpg" ? "jpeg" : "png"}"
+          width="${data.image.width}"
+          height="${data.image.height}"
+          medium="image"
+          url="${site + data.image.src}" />
+      `,
     })),
     customData: `<language>es-mx</language>`,
   });
